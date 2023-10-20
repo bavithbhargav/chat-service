70a9eb342162b6e10b87ea0488c1050b4cbbeef3
Bavith Bhargav <bbhargav@tartansense.com>
2023-10-20 11:07:39 +0530

    added the deployment Dockerfile

--- src/main/java/com/bavithbhargav/chatservice/controllers/UserController.java
+++ src/main/java/com/bavithbhargav/chatservice/controllers/UserController.java
@@ -1,15 +1,13 @@
 package com.bavithbhargav.chatservice.controllers;
 
 import com.bavithbhargav.chatservice.collections.User;
+import com.bavithbhargav.chatservice.models.ChatServiceResponseMessage;
 import com.bavithbhargav.chatservice.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/user-service", produces = "application/json")
@@ -17,6 +15,11 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/api/v1/test")
+    public ResponseEntity<ChatServiceResponseMessage> testEndpoint() {
+        return new ResponseEntity<>(new ChatServiceResponseMessage("Server is up and running"), HttpStatus.OK);
+    }
 
     @PostMapping("/api/v1/register")
     public ResponseEntity<User> registerUser(@Valid @RequestBody User user) {
