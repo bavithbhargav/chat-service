09b51fe7e7dfe98112d62eeeee0f0fea7fa0e1cb
Bavith Bhargav <bbhargav@tartansense.com>
2023-11-03 10:49:57 +0530

    added the other two cases for group PATCH request

--- src/main/java/com/bavithbhargav/chatservice/services/ChatServiceImpl.java
+++ src/main/java/com/bavithbhargav/chatservice/services/ChatServiceImpl.java
@@ -7,6 +7,9 @@ import com.bavithbhargav.chatservice.models.MemberInfo;
 import com.bavithbhargav.chatservice.repositories.GroupRepository;
 import com.bavithbhargav.chatservice.utils.MongoDBUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -22,6 +25,9 @@ public class ChatServiceImpl implements ChatService {
 
     @Autowired
     private GroupCreationEventPublisher groupCreationEventPublisher;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Override
     public Group createGroup(Group group) {
@@ -39,26 +45,38 @@ public class ChatServiceImpl implements ChatService {
 
     @Override
     public Group updateGroup(GroupRequestType requestType, Group group) {
-        Group groupFromDB = groupRepository.findByGroupName(group.getGroupName());
+        Group groupFromDB = mongoTemplate.findOne(
+                Query.query(Criteria.where("groupId").is(group.getGroupId())),
+                Group.class
+        );
         if (groupFromDB == null) {
             throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "Requested resource not found");
         }
         switch (requestType) {
             case ADD_USERS -> {
-                List<MemberInfo> usersToBeAdded = group.getMembers();
-                groupFromDB.getMembers().addAll(
-                        usersToBeAdded
-                                .stream()
-                                .filter(memberInfo -> memberInfo.getUserId() != null && memberInfo.getUserName() != null)
-                                .toList()
-                );
+                List<MemberInfo> usersToBeAdded = group.getMembers()
+                        .stream()
+                        .filter(memberInfo -> memberInfo.getUserId() != null && memberInfo.getUserName() != null)
+                        .filter(memberInfo -> !groupFromDB.getMembers().contains(memberInfo))
+                        .toList();
+                groupFromDB.getMembers().addAll(usersToBeAdded);
             }
             case REMOVE_USERS -> {
+                List<MemberInfo> usersToBeRemoved = group.getMembers()
+                        .stream()
+                        .filter(memberInfo -> memberInfo.getUserId() != null && memberInfo.getUserName() != null)
+                        .toList();
+                groupFromDB.getMembers().removeAll(usersToBeRemoved);
             }
             case UPDATE_GROUP_NAME -> {
-
+                boolean groupNameAlreadyExists = groupRepository.findByGroupName(group.getGroupName()) != null;
+                if (groupNameAlreadyExists) {
+                    throw new HttpClientErrorException(HttpStatus.CONFLICT, "Requested name already exists");
+                }
+                groupFromDB.setGroupName(group.getGroupName());
             }
         }
+        groupRepository.save(groupFromDB);
         return null;
     }
 
